d6fa9614b6a281529147002985ce90386a9bacf8
FelixFe26 <felix.fe015@gmail.com>
2023-04-17 20:59:03 -0300

    primeira-aula-python

---
+++ hello-world.py
@@ -0,0 +1,10 @@
+firstString = "water"
+secondString = "fall"
+thirdString = firstString + secondString
+print(thirdString)
+
+
+name = input (" What is your name? ")
+color = input (" What is your favorite color? ")
+animal = input (" What is your favorite animal? ")
+print("{}, you like a {} {}!" .format(name,color,animal))
---
+++ numeric-data.py
@@ -0,0 +1,31 @@
+print ("Python has three numeric types: int, float, and complex")
+
+myvalue=1
+print(myvalue)
+print(type(myvalue))
+print(str(myvalue) + " is of the data type " + str(type(myvalue)))
+
+myValue=3.14
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+myvalue=5j
+print(myvalue)
+print(type(myvalue))
+print(str(myvalue) + " is of the data type" + str(type(myvalue)))
+
+myvalue=True
+print(myvalue)
+print(type(myvalue))
+print(str(myvalue) + " is of the data type" + str(type(myvalue)))
+
+myvalue=False
+print(myvalue)
+print(type(myvalue))
+print(str(myvalue) + "is of the data type" + str(type(myvalue)))
+
+myString = "This is a  string."
+print(myString)
+print(type(myString))
+print(str(myString) + " is of the data type " + str(type(myString)))
